badac403775e11dffa036b1a4c8785ad62e733ef
zaime ali <zaimeali1997@gmail.com>
2020-10-25 02:39:47 +0500

    Working on Functionality

---
+++ src/screen/ChooseDate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function ChooseDate() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
